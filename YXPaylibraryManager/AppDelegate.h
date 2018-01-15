ab56517ca2a687623d3448df99601dd23849d678
郏国上 <jiaguoshang@geely.com>
2018-01-15 20:04:47 +0800

    1.0.1

---
+++ YXPaylibraryManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXPaylibraryManager
+//
+//  Created by 郏国上 on 2018/1/15.
+//  Copyright © 2018年 郏国上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXPaylibraryManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YXPaylibraryManager
+//
+//  Created by 郏国上 on 2018/1/15.
+//  Copyright © 2018年 郏国上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
